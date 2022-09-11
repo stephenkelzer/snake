bac96e147baa04ef34eff96836f0ac4457fd232d
Stephen Kelzer <stephen@stephenkelzer.com>
2022-09-10 22:23:55 -0500

    drop lib and create main

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use app::App;
+
+mod app;
+mod game;
+// mod cell;
+// mod collidable;
+// mod direction;
+// mod food;
+// mod game;
+// mod game_score;
+// mod game_status;
+// mod position;
+// mod snake;
+
+fn main() {
+    yew::start_app::<App>();
+}
